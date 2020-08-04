b56837c9e93adb2c9f3ffa74ab7f2f0f7783de6c
mcfinley <mcfinley@yandex.ru>
2020-08-04 14:44:29 +0300

    Update controllers to have full functionality

--- bookkeeper/src/controllers/accounts.ts
+++ bookkeeper/src/controllers/accounts.ts
@@ -8,30 +8,33 @@ export default {
    * Create an account POST method implementation
    */
   post: async (req, res) => {
-    // req.params.userId
     const asset = String(req.body.asset)
-    const ownerId = Number(req.body.ownerId)
+    const profileId = Number(req.body.profileId || req.params.profileId)
 
     /* Validate input */
-    if (_.isEmpty(asset) || !_.isFinite(ownerId)) {
-      throw new HttpError(400, 'Asset or ownerId invalid')
+    if (_.isEmpty(asset) || !_.isFinite(profileId)) {
+      throw new HttpError(400, 'Asset or profileId invalid')
     }
 
     /* Check that user exists */
     /* TODO */
 
-    res.json(await Accounts.create({ asset, ownerId }))
+    res.json(await Accounts.create({ asset, profileId }))
   },
+
   /**
    * List all the accounts. Support `paginate`, `sort` and `filter` query params.
    * Possible filters are: { asset: 'USD' },
    */
   list: async (req, res) => {
-    // req.params.userId
     const paginate = req.query.paginate as Paginate
     const sort = req.query.sort as Sort
-    const filter = req.query.filter as Filter
+    const filter = req.query.filter || {} as Filter
     const count = !!req.query.count
+
+    if (req.params.profileId) {
+      filter.profileId = req.params.profileId
+    }
 
     /* Validate the input */
     // if (!_.isObject(paginate) || !_.isObject(sort) || !_.isObject(filter)) {
@@ -40,32 +43,35 @@ export default {
 
     res.json(await Accounts.list({ paginate, sort, filter, count }))
   },
+
   /**
    * Get on specific entry
    */
   get: async (req, res) => {
-    // req.params.userId
     const accountId = Number(req.params.accountId)
+    const profileId = req.params.profileId ? Number(req.params.profileId) : undefined
 
-    res.json('TODO')
+    res.json(await Accounts.get({ id: accountId, profileId }))
   },
+
   /**
-   *
+   * Update specific account entry
    */
   update: async (req, res) => {
-    // req.params.userId
-    const patchUpdate = req.method === 'PATCH'
-    const accountId = Number(req.params.accountId)
+    // const patchUpdate = req.method === 'PATCH'
+    // const accountId = Number(req.params.accountId)
+    // const profileId = req.params.profileId ? Number(req.params.profileId) : undefined
 
-    res.json('TODO')
+    res.sendStatus(404)
   },
+
   /**
-   *
+   * Delete a specific account
    */
   delete: async (req, res) => {
-    // req.params.userId
     const accountId = Number(req.params.accountId)
+    const profileId = req.params.profileId ? Number(req.params.profileId) : undefined
 
-    res.json('TODO')
+    res.json(await Accounts.delete({ id: accountId, profileId }))
   },
 }
---
+++ bookkeeper/src/controllers/profiles.ts
@@ -0,0 +1,60 @@
+import _ from 'lodash'
+
+import { HttpError, Paginate, Sort, Filter } from '../libs/utils'
+import Profiles from '../models/profiles'
+
+export default {
+  /**
+   * Create a profile
+   */
+  post: async (req, res) => {
+    res.json(await Profiles.create())
+  },
+
+  /**
+   * List all the accounts. Support `paginate`, `sort` and `filter` query params.
+   * Possible filters are: { asset: 'USD' },
+   */
+  list: async (req, res) => {
+    const paginate = req.query.paginate as Paginate
+    const sort = req.query.sort as Sort
+    const filter = req.query.filter || {} as Filter
+    const count = !!req.query.count
+
+    res.json(await Profiles.list({ paginate, sort, filter, count }))
+  },
+
+  /**
+   * Get on specific profile entry
+   */
+  get: async (req, res) => {
+    const profileId = Number(req.params.profileId)
+
+    if (!_.isFinite(profileId)) {
+      throw new HttpError(400, 'Invalid ID')
+    }
+
+    res.json(await Profiles.get(profileId))
+  },
+
+  /**
+   * Update a profile entry
+   */
+  update: async (req, res) => {
+    /* There are no fields that can be updated */
+    res.sendStatus(200)
+  },
+
+  /**
+   * Delete a profile entry
+   */
+  delete: async (req, res) => {
+    const profileId = Number(req.params.profileId)
+
+    if (!_.isFinite(profileId)) {
+      throw new HttpError(400, 'Invalid ID')
+    }
+
+    res.json(await Profiles.delete(profileId))
+  },
+}
